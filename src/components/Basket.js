affdcb01c613a000bfbe51da2f4743e5fd8c0644
Niels Louwes <niels.louwes@and.digital>
2022-02-15 17:57:13 +0100

    Added basic layout

---
+++ src/components/Basket.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Basket() {
+  return (
+    <aside className="block col-1">
+      Basket
+      <h2> Cart Items</h2>
+    </aside>
+  );
+}
+
+export default Basket;
