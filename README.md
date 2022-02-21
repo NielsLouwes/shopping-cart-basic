bf8167829e28c1a12f39c9a3b1a669fc82912b87
Niels Louwes <niels.louwes@and.digital>
2022-02-21 22:00:51 +0100

    Added cart open and close functionality

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 // 1. Want to add a cart that drops down
 // 2. Want to add checkout page
 // 3. Want to add way to pay
-// 4. Want to add logging in with local storage
-// 5. Want to add 
+// 4. Want to add logging in with local storage - https://www.stackovercloud.com/2020/12/03/how-to-add-login-authentication-to-react-applications/
+// 5. Want to add context for state
+
+
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Divider from '@mui/material/Divider';
+
+const style = {
+  position: 'absolute',
+  top: '7%',
+  left: '70%',
+  width: 400,
+  height: 400,
+  bgcolor: 'background.paper',
+  // border: '1px solid #000',
+  borderRadius: '15px',
+  boxShadow: 24,
+  p: 4
+};
 
 function Header({ countCartItems }) {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <header className="row block center">
       <div>
@@ -9,9 +30,30 @@ function Header({ countCartItems }) {
         </a>
       </div>
       <div className="cart-sign-in">
-        <a href="#/cart">Cart {countCartItems ? <button>{countCartItems}</button> : ''}</a>{' '}
+        <a href="#/cart" onClick={handleOpen}>
+          Cart {countCartItems ? <button>{countCartItems}</button> : ''}
+        </a>{' '}
         <a href="#/signin">Sign In</a>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description">
+        <Box sx={style}>
+          <Typography
+            style={{ fontWeight: 'bolder', color: 'black' }}
+            id="modal-modal-title"
+            variant="h5"
+            component="h2">
+            Shopping cart
+          </Typography>
+          <Divider style={{ marginTop: '10px' }} />
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Your cart is empty.
+          </Typography>
+        </Box>
+      </Modal>
     </header>
   );
 }
