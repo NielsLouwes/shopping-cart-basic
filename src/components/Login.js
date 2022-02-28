d9a9c67d605db456754bf10d08f309e6cc6c3373
Niels Louwes <niels.louwes@and.digital>
2022-02-28 19:36:24 +0100

    Added form page style

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  // const [invalidPassword, setInvalidPassword] = useState(false);
+  // const { register, handleSubmit } = useForm();
+  // const navigate = useNavigate();
+
+  // const onSubit = ({ password, email }) => {
+  //   setInvalidPassword(false);
+  //   const myStorage = window.localStorage;
+  //   if (password === 'password' && email === 'email.nl') {
+  //     myStorage.setItem('loggedIn', 'true');
+  //   }
+  // };
+
   return (
-    <div>
-      <form action="/action_page.php" />
-      <label htmlFor="fname">First name:</label>
+    <div className="form-body">
+      <div className="form-main">
+        <label htmlFor="chk" aria-hidden="true">
+          Login
+        </label>
 
-      <input type="text" id="fname" name="fname" value="John" />
-      <label htmlFor="lname">Last name:</label>
-
-      <input type="text" id="lname" name="lname" value="Doe" />
-      <input type="submit" value="Submit" />
+        <input type="checkbox" id="chk" aria-hidden="true" />
+        <div className="login">
+          <form>
+            {/* <label htmlFor="chk" aria-hidden="true">
+              Login
+            </label> */}
+            <input
+              type="email"
+              {...register('password', { required: true })}
+              placeholder="Password"
+            />
+            <input type="password" name="pswd" placeholder="Password" required="true" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
